2dcf768c37f3d237a976ad5095c466779e716113
Jonathan Lermitage <jonathan.lermitage@gmail.com>
2021-11-26 15:45:05 +0100

    v1.11

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,6 @@
 ## Icon Viewer 2 Change Log
 
-### 1.11.0 (WIP)
+### 1.11.0 (2021/11/26)
 * improve SVG support. Thx [cherepanov](https://github.com/jonathanlermitage/IconViewer/pull/4).
 
 ### 1.10.0 (2021/07/01)
--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,7 @@ val twelvemonkeysVersion = "3.7.0"
 
 val inCI = System.getenv("CI") != null
 
-println("Will use IDEA $pluginIdeaVersion and Java $pluginJavaVersion")
+logger.quiet("Will use IDEA $pluginIdeaVersion and Java $pluginJavaVersion")
 
 group = "lermitage.intellij.iconviewer"
 version = pluginVersion
@@ -72,17 +72,28 @@ tasks {
             componentSelection {
                 all {
                     if (isNonStable(candidate.version)) {
-                        println(" - [ ] ${candidate.module}:${candidate.version} candidate rejected")
+                        logger.debug(" - [ ] ${candidate.module}:${candidate.version} candidate rejected")
                         reject("Not stable")
                     } else {
-                        println(" - [X] ${candidate.module}:${candidate.version} candidate accepted")
+                        logger.debug(" - [X] ${candidate.module}:${candidate.version} candidate accepted")
                     }
                 }
             }
         }
     }
     runIde {
-        jvmArgs = listOf("-Xms768m", "-Xmx2048m", "--add-exports", "java.base/jdk.internal.vm=ALL-UNNAMED")
+        jvmArgs("-Xms128m")
+        jvmArgs("-Xmx1024m")
+        jvmArgs("--add-exports", "java.base/jdk.internal.vm=ALL-UNNAMED")
+        // copy over some JVM args from IntelliJ
+        jvmArgs("-Dide.no.platform.update=true")
+        jvmArgs("-Djdk.attach.allowAttachSelf=true")
+        jvmArgs("-Djdk.module.illegalAccess.silent=true")
+        jvmArgs("-Dsun.io.useCanonCaches=false")
+        jvmArgs("-XX:+UseG1GC")
+        jvmArgs("-XX:CICompilerCount=2")
+        jvmArgs("-XX:ReservedCodeCacheSize=512m")
+        jvmArgs("-XX:SoftRefLRUPolicyMSPerMB=50")
     }
     buildSearchableOptions {
         enabled = pluginEnableBuildSearchableOptions.toBoolean()
