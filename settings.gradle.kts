71394cf30c8024a47d2d4fb4b4426e103fa0adda
Jonathan Lermitage <jonathan.lermitage@gmail.com>
2022-11-25 23:34:36 -0500

    fix build

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,8 @@
 rootProject.name = "ij-iconviewer"
+
+buildscript {
+    repositories {
+        // don't use jcenter.bintray.com (Jfrog)
+        maven(url = "https://repo1.maven.org/maven2")
+    }
+}
